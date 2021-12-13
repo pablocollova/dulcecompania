7b834b5d68c59fbaf9758d22236b46d85e8efb61
Pablo Ariel Collova <pablocollova@gmail.com>
2021-12-13 13:15:28 -0300

    prueba

--- index.php
+++ index.php
@@ -14,6 +14,6 @@
  * @var bool
  */
 define( 'WP_USE_THEMES', true );
-
+echo "prueeeeeeeeeeeeeeeeeeeeeeebaaaaaaaaaaaaaaaaaa";
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
